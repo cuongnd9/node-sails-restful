eede19412d70fad745e9bb76ca61126dc22871c9
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-04-30 00:14:58 +0700

    Completed Restful API

--- api/controllers/CatController.js
+++ api/controllers/CatController.js
@@ -6,10 +6,73 @@
  */
 
 module.exports = {
-  getCats: async (req, res) => {},
-  createCat: async (req, res) => {},
-  getCat: async (req, res) => {},
-  updateCat: async (req, res) => {},
-  deleteCat: async (req, res) => {}
+  getCats: async (req, res) => {
+    Cat.find().exec((err, cats) => {
+      if (err) {
+        return res.serverError({
+          success: false,
+          message: 'Server error'
+        });
+      }
+      return res.json(200, { result: cats });
+    });
+  },
+  createCat: async (req, res) => {
+    const { name, color } = req.body;
+    Cat.create({ name, color }).exec(err => {
+      if (err) {
+        return res.serverError({
+          success: false,
+          message: 'Server error'
+        });
+      }
+      return res.ok({
+        success: true,
+        message: 'Cat was created'
+      });
+    });
+  },
+  getCat: async (req, res) => {
+    const { id } = req.params;
+    Cat.findOne({ id }).exec((err, cat) => {
+      if (err) {
+        return res.serverError({
+          success: false,
+          message: 'Server error'
+        });
+      }
+      return res.json(200, { result: cat });
+    });
+  },
+  updateCat: async (req, res) => {
+    const { id } = req.params;
+    const { name, color } = req.body;
+    Cat.updateOne({ id }, { name, color }).exec(err => {
+      if (err) {
+        return res.serverError({
+          success: false,
+          message: 'Server error'
+        });
+      }
+      return res.ok({
+        success: true,
+        message: 'Cat was updated'
+      });
+    });
+  },
+  deleteCat: async (req, res) => {
+    const { id } = req.params;
+    Cat.destroy({ id }).exec(err => {
+      if (err) {
+        return res.serverError({
+          success: false,
+          message: 'Server error'
+        });
+      }
+      return res.ok({
+        success: true,
+        message: 'Cat was deleted'
+      });
+    });
+  }
 };
-
